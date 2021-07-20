65e047ddc4f4966c1ba016aa039aae42c855115d
Timo <38291523+lovetodream@users.noreply.github.com>
2021-07-20 21:30:36 +0200

    ✨ Add Initials View

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "InitialsUI",
+    platforms: [.macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
--- Sources/InitialsUI/InitialsUI.swift
+++ Sources/InitialsUI/InitialsUI.swift
@@ -1,6 +1,99 @@
-public struct InitialsUI {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct InitialsUI<Content: View>: View {
+    @Binding var text: String
+    
+    let background: Content
+    
+    var initials: String {
+        text.components(separatedBy: .whitespaces)
+            .filter { !$0.isEmpty }
+            .reduce("") { partialResult, word in
+                partialResult + String(word.first!.uppercased())
+        }
+    }
+    
+    init(text: Binding<String>, @ViewBuilder background: @escaping () -> Content) {
+        self.background = background()
+        self._text = text
+    }
+    
+    public var body: some View {
+        GeometryReader { g in
+            ZStack {
+                background
+                    
+                Text(initials)
+                    .modifier(FitToWidth())
+                    .padding()
+            }
+        }
+    }
+}
+
+extension InitialsUI {
+    init(initials: String, @ViewBuilder background: @escaping () -> Content) {
+        let text = initials.map { "\($0)" }.joined(separator: " ")
+        
+        self.init(text: .constant(text), background: background)
+    }
+}
+
+extension InitialsUI where Content == Color {
+    init(text: Binding<String>) {
+        self.init(text: text) {
+            Color.gray
+        }
+    }
+    
+    init(initials: String) {
+        self.init(initials: initials) {
+            Color.gray
+        }
+    }
+}
+
+struct FitToWidth: ViewModifier {
+    var fraction: CGFloat = 1.0
+    func body(content: Content) -> some View {
+        GeometryReader { g in
+            VStack {
+                Spacer()
+                content
+                    .font(.system(size: 1000))
+                    .minimumScaleFactor(0.005)
+                    .lineLimit(1)
+                    .frame(width: g.size.width*self.fraction)
+                Spacer()
+            }
+        }
+    }
+}
+
+
+// MARK: - Development only
+
+struct InitialsUIWrapper: View {
+    @State var name = ""
+
+    public var body: some View {
+        VStack {
+            TextField("Name", text: $name)
+        
+            InitialsUI(text: $name)
+        }
+    }
+}
+
+struct InitialsUI_Previews: PreviewProvider {
+    static var previews: some View {
+        InitialsUIWrapper()
+        InitialsUI(text: .constant("Timo Zacherl")) {
+            RadialGradient(colors: [.gray, .blue], center: .trailing, startRadius: 40, endRadius: 180)
+        }
+        InitialsUI(initials: "TZ") {
+            Color.yellow
+        }.foregroundColor(.white)
+        InitialsUI(initials: "TZ")
     }
 }
